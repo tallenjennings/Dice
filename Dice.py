f6dba7fd8226366ee03e9461b3e374619a626437
Trevor Jennings <tallenjennings@hotmail.com>
2019-04-30 15:00:00 -0600

    fixed formatting and added a dice func call

--- Dice.py
+++ Dice.py
@@ -1,24 +1,29 @@
 """This program will roll dice"""
 
-#import modules
+#Import modules
 import random
 
-#define min and max numbers on dice
-#define how many dice
-min = 1
-max = int(input('How many sides should be on the dice?'))
-dice = int(input('How many dice shall I roll?'))
+#define dice function
+def dice():
+    
+    #Define min and max numbers on dice
+    min = 1
+    max = int(input('How many sides should be on the dice?'))
+    
+    #Define how many dice
+    dice = int(input('How many dice shall I roll?'))
 
-#play again variable
-rollAgain = 'y'
-
-#rolling the dice while loop
-while rollAgain.lower() == 'y':
+    #Rolling the dice while loop
     print('Rolling the Dice!')
     print('The Dice are...')
     for number in range(0, dice):
         print(random.randint(min, max))
-    rollAgain = str(input('Do you want to roll again?y/n:'))
+    
+
+#Calling the dice game
+rollAgain = 'y'
+while rollAgain == 'y':
+    dice()
+    rollAgain = input('Do you want to roll again?y/n:')
     if rollAgain == 'n':
         break
-
